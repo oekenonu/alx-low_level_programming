a1448a6b5af229d6e4c79fd344b4955d7fd8d256
obinna E <obinnaekenonu@gmail.com>
2023-11-22 21:54:12 +0000

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines the island perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """Return perimiter of the island.
+    0 Represents a water zone
+    1 represents a land zone
+
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
