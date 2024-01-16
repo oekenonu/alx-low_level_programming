153af51865e1f6db1e3ee9939d85279d257dd8ec
obinna E <obinnaekenonu@gmail.com>
2024-01-16 21:47:58 +0000

    2-O

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+
+/**
+ * linear_search: Search an array linearly
+ * @array: Array to be searched
+ * @size: Size of array
+ * @value: Value to search for
+ * Return: first index where value is located otherwise 
+ *         -1 if value is not present or array is NULL
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int index;
+	for (index = 0; array && index < size; index++)
+	{
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+		{
+			return (index);													}
+	}
+	return (-1);
+}
